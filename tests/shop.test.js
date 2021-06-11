d76ca280546d395ef15fcbeeea88719887d5798e
Everton <58737540+everton-nasc@users.noreply.github.com>
2021-06-11 16:49:23 -0600

    dummy test - pr check

---
+++ tests/shop.test.js
@@ -0,0 +1,13 @@
+const shop = require('../routes/shop');
+const express = require('express');
+const request = require('supertest');
+
+const app = express();
+
+describe('Admin Route Tests', function() {
+    it('should get 200 code from shop', () => {
+        app.use(express.urlencoded({ extended: false}));
+        app.use("/", shop);
+    });
+
+});
